a3a097fb35a5f65efd221a8dff1eebedfcdb7395
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@CFP127ALUNO.LOCAL>
2025-04-17 12:27:12 -0300

    Inicio da programação do front pro back

--- JS/jogo.js
+++ JS/jogo.js
@@ -1,2 +1,24 @@
 'user strict'
 
+async function InserirJogo(jogo){
+    const url = `http://localhost:3030/v1/controle-jogos/jogo/`
+    
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            jogo
+        )
+    }
+
+    
+
+    return data
+}
+
+
+const novoJogo {
+    // Passar aq o Json da Requisição 
+}
