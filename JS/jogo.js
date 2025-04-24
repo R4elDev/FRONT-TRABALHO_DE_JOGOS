7f824818240a16446517a8e5b46ce141cd328c88
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@CFP127ALUNO.LOCAL>
2025-04-24 11:37:33 -0300

    realizado o js de jogo

--- JS/jogo.js
+++ JS/jogo.js
@@ -1,24 +1,115 @@
 'user strict'
 
-async function InserirJogo(jogo){
+
+// Fazer um post de jogo
+async function inserirJogo(){
     const url = `http://localhost:3030/v1/controle-jogos/jogo/`
+
+    const data = {
+        // Valor dos inputs no formulario
+
+
+        "nome": "Mortal Kombat",
+        "data_lancamento": "1985-09-13",
+        "versao": "1.0",
+        "tamanho": "500KB",
+        "descricao": "Jogo bem legal para diversão",
+        "foto_capa": "http://foto.jpg",
+        "link": "http://downloadJogo.zip"
+    }
     
     const options = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(
-            jogo
-        )
+        body: JSON.stringify(data)
     }
 
+    const response = await fetch(url,options)
+
+    console.log(response)
+
+    if(response.status == 201){
+        alert('Postagem Criada')
+    }
+}
+
+// Fazer o GET de TODOS os Jogos
+async function pesquisarJogos() {
+    const url = `http://localhost:3030/v1/controle-jogos/jogo`
+    const response = await fetch(url)
+
+
+    const data = await response.json()
+
+    console.log(data.games)
+    return data.games
+}
+
+// Fazer o GET de um jogo pelo ID
+async function pesquisarJogoPorID(id) {
+    const url = `http://localhost:3030/v1/controle-jogos/jogo/${id}`
+    const response = await fetch(url)
+
+
+    const data = await response.json()
+
+    return data.games
+}
+
+
+async function excluirUmJogoPorID(id) {
+    const url = `http://localhost:3030/v1/controle-jogos/jogo/${id}`
+
+    const options = {
+        method : 'DELETE'
+    }
+
+    const response = await fetch(url,options)
+
+    console.log(response)
+    if(response.status == 200){
+        alert('POSTAGEM DELETADA')
+    }
+
+    return response.ok
+}
+
+// Realizando o PUT em um jogo por ID
+async function atualizarJogoPorID(id){
+    const url = `http://localhost:3030/v1/controle-jogos/jogo/${id}`
+
+    const data = {
+        // Valor dos inputs no formulario
+        "nome": "Mortal Kombat 3",
+        "data_lancamento": "1985-09-13",
+        "versao": "1.0",
+        "tamanho": "500KB",
+        "descricao": "Jogo bem legal para diversão",
+        "foto_capa": "http://foto.jpg",
+        "link": "http://downloadJogo.zip"
+    }
     
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
 
-    return data
+    const response = await fetch(url,options)
+
+    console.log(response)
+
+    if(response.status == 201){
+        alert('Postagem Atualizada')
+    }
 }
 
 
-const novoJogo {
-    // Passar aq o Json da Requisição 
-}
+
+
+
+
